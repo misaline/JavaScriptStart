5345cbe2e5ecc0450744a8c4e4e1e492a345ed09
misaline <102043612+misaline@users.noreply.github.com>
2023-02-06 12:10:01 +0300

    javascript start

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("merhaba");
+console.log(1234);
+console.log(true);
+console.log([1, 2, 3]);
+console.error("hata");
+console.warn("uyarı");
+//tek satıt yporum
+
+/*
+
+    yorums atyttıyrhtjmnh
+    fmjlkmgd,
+    gmrfel
+
+*/
+console.clear();
+
+

0825dde15ef627f28e70eb74fa2735c924d5e3a2
misaline <102043612+misaline@users.noreply.github.com>
2023-02-09 15:13:57 +0300

    operator ornekleri

--- JsStart/date&timesOrnek.js
+++ JsStart/date&timesOrnek.js
@@ -16,6 +16,44 @@ dtC.setDate(dayOfMonth - 1); // günden -1 yaptık bir önceki ayın son günüe
 console.log(dtC);
 
 // ** İki tarih arasındaki geçen zamanı bulunuz.
+let date1 = new Date("02/09/2023");
+let date2 = new Date("02.09.2022");
+let milisecond = date1 - date2;
+let saniye = milisecond / 1000;
+let dakika = saniye / 60;
+let saat = dakika / 60;
+let gun = saat / 24;
 
-// ** Her yıl mayıs ayının 2. haftası pazar günü kutlanan anneler günü 2023 yılında ne zaman kutlanacaktır?
+
+console.log("milisaniye: " + milisecond);
+console.log("saniye: " + saniye);
+console.log("dakika: " + dakika);
+console.log("saat: " + saat);
+console.log("gun: " + gun);
+
+
+
+// ** Her yıl mayıs ayının 2. haftası pazar günü kutlanan anneler günü
+//    2023 yılında ne zaman kutlanacaktır?
+let motherDay = new Date();
+motherDay.setHours(0, 0, 0, 0); // saatle işimiz olmadığı için sıfırladık
+motherDay.setFullYear(2023); // hangi yıldaki anneler gününü istediğimiz
+motherDay.setDate(1); // 1. güne konumlandırdık /mayısın 1. günü
+motherDay.setMonth(4); // mayıs istedik 
+
+while (motherDay.getDay() != 0) { // (while= iken motherDay getDay(bana dönen değer) 0 yani pazar değilse 
+    motherDay.setDate(motherDay.getDate() + 1) //yeni gün arayışına giriyor. 0 a eşit olana kadar 1 ekliyoruz.
+}
+motherDay.setDate(motherDay.getDate() + 7); //1. hafta değil 2. haftaya denk gelsin diye 7 gün ekledik. 
+console.log(motherDay);
+
+
+
 // ** Yaş hesaplama nasıl yapılır?
+let birthDay = new Date("06/10/1996");
+let ageDifMS = Date.now() - birthDay.getTime();//şimdiki milsecondtan birthday milisecond cinsinden çıkardık.
+let ageDate = new Date(ageDifMS);
+console.log(ageDate.getFullYear() - 1970); //Milisecond başlangıç tarihi 1970dir. yaş hesaplamasında 1970 i çıkarmak zorundayız bu yüzden.
+
+// console.log(birthDay.getTime()); //milisaniye cinsinden gösterir 1970 den yazdığımız tarihe kadar milisaniye gösterir.
+// console.log(Date.now()) // şimdiki tarihin  milisecond cinsinden gösterir.
